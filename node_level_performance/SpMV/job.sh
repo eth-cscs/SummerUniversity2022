96a7e369b4bceb85cf91b431818e61436983977a
Georg Hager <georg.hager@fau.de>
2022-07-23 10:05:59 +0200

    job script

--- node_level_performance/SpMV/job.sh
+++ node_level_performance/SpMV/job.sh
@@ -1,13 +1,15 @@
 #!/bin/bash -l
 #SBATCH --job-name=spmv
-#SBATCH --time=00:05:00
+#SBATCH --time=00:25:00
 #SBATCH --nodes=1
 #SBATCH --constraint="gpu&perf"
 #SBATCH --account=class07
 
-export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 export CRAY_CUDA_MPS=1
 
 source ./modules.sh
 
-srun ./spmv-gpu /scratch/snx3000/class355/ghager/matrices/HPCG-192-192-192.bmtx scs -dp -c 32 -s 32 -no-verify
+echo Working qcd5_4
+srun ./spmv-gpu $MATRICES/qcd5_4.mtx csr -dp -c 32 -s 32 -no-verify
+srun ./spmv-gpu $MATRICES/qcd5_4.mtx scs -dp -c 32 -s 32 -no-verify
+
--- node_level_performance/SpMV/modules.sh
+++ node_level_performance/SpMV/modules.sh
@@ -1,3 +1,5 @@
 module switch PrgEnv-cray/6.0.10 PrgEnv-gnu
 module load cdt-cuda/22.05
 module load nvhpc-nompi/22.2
+export MATRICES=/scratch/snx3000/class355/ghager/matrices
+
